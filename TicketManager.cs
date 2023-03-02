4b2ee6b6100b53deddc8adabbbe8f85e015b321b
devisriManavarthi <125434898+devisriManavarthi@users.noreply.github.com>
2023-03-01 23:49:59 -0700

    added new feature to antme colony virtual game

---
+++ TicketManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntMe.Player.FirstAntColony
+{
+    public class TicketManager
+    {
+        #region Singleton
+        private static TicketManager _instance;
+
+        public static TicketManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new TicketManager();
+                return _instance;
+            }
+        }
+        #endregion
+
+        private List<English.Sugar> sweet = new List<English.Sugar>();
+        private List<FirstAntColonyClass> myant = new List<FirstAntColonyClass>();
+        internal void ReportSugar(English.Sugar sugar)
+        {
+            //throw new NotImplementedException();
+            if (!sweet.Contains(sugar))
+            {
+                sweet.Add(sugar);
+
+            }
+        }
+
+        //internal void RegisterAnt(FirstAntColonyClass myant)
+        //{
+        //    if (!myant.Contains(myant))
+        //    {
+        //        myant.Add(myant);
+        //    }
+        //}
+    }
+}
